17add0bfb097de74ee9608ae9aec2f037f54cf15
Abraham Panduro <panduroab@gmail.com>
2017-11-05 11:44:05 -0600

    ProtectedRoute component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './views/home';
 import Login from './views/login';
 import SignUp from './views/signup';
 import ForgotPassword from './views/forgot-password';
 import Dashboard from './views/dashboard';
-
+import AuthService from './services/AuthService';
+const auth = new AuthService();
+class ProtectedRoute extends Component {
+  render() {
+    const comp = (auth.isAuthenticated()) ?
+      <Route {...this.props} /> :
+      <Redirect to={{
+        pathname: '/',
+        state: { from: this.props.location }
+      }} />;
+    return comp;
+  }
+}
 export default class App extends Component {
   render() {
     return (
@@ -16,7 +28,7 @@ export default class App extends Component {
           <Route path="/login" component={Login} />
           <Route path="/signup" component={SignUp} />
           <Route path="/forgot-password" component={ForgotPassword} />
-          <Route path="/dashboard" component={Dashboard} />
+          <ProtectedRoute path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
     );
